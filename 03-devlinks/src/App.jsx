94080907ec8db613492fbaefbd72d34e2615ce88
hillarycacheiro <hillarycacheiro98@gmail.com>
2026-02-12 13:53:34 -0300

    feat: mover componente Perfil para seu próprio arquivo e remover código redundante

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -10,11 +10,6 @@ function App() {
       <Perfil />
 
 
-
-      <div className='perfil'>
-        <img src="https://placehold.co/200x200" alt="" />
-        <p>@Seu Nome</p>
-      </div>
       <div className="switch">
         botão switch
       </div>
