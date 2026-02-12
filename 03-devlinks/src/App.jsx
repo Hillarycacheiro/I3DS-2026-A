71c161364ab2adaf977acd32338f809c2d937700
hillarycacheiro <hillarycacheiro98@gmail.com>
2026-02-12 16:58:40 -0300

    feat: adicionar componentes Link, Rodape e SocialLink; atualizar componente Perfil e estrutura do App

--- 03-devlinks/src/App.jsx
+++ 03-devlinks/src/App.jsx
@@ -1,21 +1,32 @@
 import './App.css'
+import Link from './components/Link/Link';
 import Perfil from './components/Perfil/Perfil';
+import Rodape from './components/Rodape/Rodape';
+import SocialLink from './components/SocialLink/SocialLink';
 
 
 function App() {
-  
-
   return (
     <div id='App'>
-      <Perfil />
+      <Perfil fotoPerfil={"https://placehold.co/100"} >Hillary Cacheiro</Perfil>
 
+      <div className="switch">botão switch</div>
 
-      <div className="switch">
-        botão switch
-      </div>
-      <div className='links'></div>
+      <ul>
+        <Link url={""}>Inscreva-se</Link>
+        <Link url={""}>Minha Playlist</Link>
+        <Link url={""}>Me pague um lanche</Link>
+        <Link url={""}>Conheça o curso de DEV</Link>
+      </ul>
+
       <div className='socialLinks'></div>
-      <div className='rodape'></div>
+      <SocialLink url={"https://github.com"} icon={"logo-github"}/>
+      <SocialLink url={"https://instagram.com"} icon={"logo-instagram"}/>
+      <SocialLink url={"https://youtube.com"} icon={"logo-youtube"}/>
+      <SocialLink url={"https://br.linkedin.com"} icon={"logo-linkedin"}/>
+
+      <Rodape>HillaryCacheiro</Rodape>
+
     </div>
   )
 }
