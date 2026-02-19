4be394aa67b9e32452076176e303e58d4d5ab538
hillarycacheiro <hillarycacheiro98@gmail.com>
2026-02-19 11:18:52 -0300

    feat: implementar componente Rodape e atualizar estrutura do App

--- 04-devflix/src/App.jsx
+++ 04-devflix/src/App.jsx
@@ -1,9 +1,26 @@
 import "./App.css";
 
+import logo from "./assets/devflix.png";
+import lupa from "./assets/search.svg";
+import Rodape from "./components/Rodape/Rodape";
+
 const App = () => {
   return (
-    <div>App</div>
-  )
-}
+    <div id="App">
+      <img
+        id="logo"
+        src={logo}
+        alt="Logo da plataforma de streaming DEVFLIX em destaque, com cores vibrantes e design moderno, ideal para quem busca serviços de entretenimento online."
+      />
 
-export default App
+      <div className="search">
+        <input type="text" placeholder="Pesquise por filmes e séries..." />
+        <img src={lupa} alt="Botão de ação para pesquisa!" />
+      </div>
+
+      <Rodape>HillaryCacheiro</Rodape>
+    </div>
+  );
+};
+
+export default App;
