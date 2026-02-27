13f4ab18a2a54090290b06bf4441187c6754baad
hillarycacheiro <hillarycacheiro98@gmail.com>
2026-02-27 15:45:33 -0300

    Cria API REST inicial com EF Core, Scalar e SQL Server
    Adiciona estrutura de solução ASP.NET Core 10, modelos de domínio (Aluno, Curso, Disciplina, AlunoCurso), contexto de dados, controllers CRUD, configuração de banco SQL Server LocalDB, documentação OpenAPI/Scalar e migrações iniciais do Entity Framework Core. Inclui arquivos de configuração, .gitignore e endpoint de status da API.

---
+++ PrimeiraAPI/PrimeiraAPI/Models/Disciplina.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PrimeiraAPI.Models
+{
+    public class Disciplina
+    {
+        public Guid DisciplinaId { get; set; }
+        [Required]
+        public string? Nome { get; set; }
+        public int CargaHoraria { get; set; }
+        public int Semestre { get; set; }
+    }
+}
