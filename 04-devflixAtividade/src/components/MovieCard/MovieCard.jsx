7de0318dc6008f1ae9275a672159de75c1dc4615
hillarycacheiro <hillarycacheiro98@gmail.com>
2026-03-06 11:28:05 -0300

    feat: atualizar estilos do tema e adicionar funcionalidade de tradução automática no MovieCard

--- 04-devflixAtividade/src/components/MovieCard/MovieCard.jsx
+++ 04-devflixAtividade/src/components/MovieCard/MovieCard.jsx
@@ -1,14 +1,41 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./MovieCard.module.css";
 import MovieDescription from "../MovieDescription/MovieDescription";
 
 const MovieCard = (props) => {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [translatedTitle, setTranslatedTitle] = useState(props.Title);
   // console.log(isModalOpen);
 
   const toggleModal = () => {
     setIsModalOpen(!isModalOpen);
   };
+
+  const translateText = async (text, targetLanguage = "pt") => {
+    try {
+      const url = `https://translate.googleapis.com/translate_a/single?client=gtx&sl=en&tl=${targetLanguage}&dt=t&q=${encodeURIComponent(text)}`;
+      const response = await fetch(url);
+      const data = await response.json();
+      return data[0].map((item) => item[0]).join("");
+    } catch (error) {
+      console.error("Erro ao traduzir título:", error);
+      return text;
+    }
+  };
+
+  useEffect(() => {
+    const updateTitle = async () => {
+      if (props.language === "en") {
+        setTranslatedTitle(props.Title);
+        return;
+      }
+
+      const translated = await translateText(props.Title, props.language);
+      setTranslatedTitle(translated);
+    };
+
+    updateTitle();
+  }, [props.language, props.Title]);
 
   return (
     <>
@@ -23,7 +50,7 @@ const MovieCard = (props) => {
 
         <div>
           <span>{props.Type}</span>
-          <h3>{props.Title}</h3>
+          <h3>{translatedTitle}</h3>
         </div>
       </div>
 
